3b389ba1b526cafca2a9c50a204144ac936817e8
minul45 <89403990+minul45@users.noreply.github.com>
2021-08-23 21:00:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+main.py
+import os
+import telebot
+bot = telebot.TeleBot("APT Key Here")
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+     bot.reply_to(message,"hello i'm minul chat bot")
+
+@bot.message_handler(commands=["how"])
+def send message(message):
+     bot.send_message(message, "join telegram grouphttps://t.me/THESLGAMING12363")
+
+bot.polling()
